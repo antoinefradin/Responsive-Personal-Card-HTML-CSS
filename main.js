adbb51c7825cb7e49f5a252153b94159e82884e5
Antoine <fradin.antoine@yahoo.fr>
2022-01-01 11:57:35 +0100

    last commi

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
-/*===== MENU SHOW =====*/
+/*==== MENU SHOW ====*/
 /* Validate if constant exists */
 if(navToggle){
     navToggle.addEventListener('click',()=>{
@@ -11,7 +11,7 @@ if(navToggle){
     })
 }
 
-/*===== MENU HIDDEN =====*/
+/*==== MENU HIDDEN ====*/
 /* Validate if constant exists */
 if(navClose){
     navClose.addEventListener('click',()=>{
@@ -20,7 +20,7 @@ if(navClose){
 }
 
 
-/*===== REMOVE MOBILE MENU =====*/
+/*==== REMOVE MOBILE MENU ====*/
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
@@ -29,3 +29,47 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click',linkAction)) 
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+//highlight menu icon
+/********************************************************************/
+/************* A ADAPTER EN FONCTION DES PAGES **********************/
+/********************************************************************/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current=> {
+        const sectionHeight = current.offsetHeight; /*La valeur en pixels de la hauteur totale de l'élément identifé par id*/
+        const sectionTop = current.offsetTop - 50; /*Returns the top position (in pixels) relative to the top of the offsetParent element*/
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll',scrollActive)
+/********************************************************************/
+/********************************************************************/
+/********************************************************************/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+    const nav = document.getElementById('header');
+    //  When the scroll is greater than 80 viewport height, add the scroll-header class
+
+    if(this.scrollY >= 80){ //'window' is the whole web page. 'this' depends on the function you are in.
+        nav.classList.add('scroll-header')
+    }else{
+        nav.classList.remove('scroll-header')
+    }
+}
+window.addEventListener('scroll',scrollHeader)
+
+
